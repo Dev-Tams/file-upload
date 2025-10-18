e6d87b8f4ed6d3369b05f145ab2da4c9a15a14ac
Dev-Tams <tammyalemu@gmail.com>
2025-10-18 21:24:26 +0100

    added soft delete

--- models/User.go
+++ models/User.go
@@ -1,11 +1,16 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	ID        string `gorm:"type:uuid;primaryKey"`
 	Email     string `gorm:"uniqueIndex;not null"`
 	Password  string `gorm:"not null"`
-	Role     string `gorm:"default:user"`
+	Role      string `gorm:"default:user"`
 	CreatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
