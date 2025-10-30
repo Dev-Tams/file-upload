5361f36747bb1f821b906edec76c34b999758928
Dev-Tams <tammyalemu@gmail.com>
2025-10-30 21:48:53 +0100

    feat add db methods

--- internal/repositories/file_repository.go
+++ internal/repositories/file_repository.go
@@ -9,21 +9,37 @@ type FileRepository struct {
 	db *gorm.DB
 }
 
-func NewFileRepository (db *gorm.DB) *FileRepository{
+var file models.File
+
+func NewFileRepository(db *gorm.DB) *FileRepository {
 	return &FileRepository{db: db}
 }
 
-func (r *FileRepository) Create(file *models.File) error{
+func (r *FileRepository) Create(file *models.File) error {
 	return r.db.Create(file).Error
 }
 
+func (r *FileRepository) GetById(id, userID string) (*models.File, error) {
+		err := r.db.
+		Where("id = ? AND user_id = ?", id, userID).
+		Preload("User").
+		First(&file).Error
 
-func (r *FileRepository) GetById(userID, id string, find any) (any, error) {
-
-	err :=  r.db.Where("id = ? AND user_id = ?", id, userID).Preload("User").First(&find).Error
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
-	return &find, nil
+
+	return &file, nil
+}
+
+func (r *FileRepository) Delete(delete *models.File) error {
+	if err := r.db.Delete(delete).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *FileRepository) GET(userID string) error{
+	return r.db.Where("user_id = ?", userID).Order("uploaded_at DESC").Preload("User").Error
 
 }
