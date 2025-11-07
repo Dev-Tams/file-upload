50a2de536bfe046893762f551d56758f32ca5cdc
Dev-Tams <tammyalemu@gmail.com>
2025-11-07 22:21:49 +0100

    refactor model to suite app design

--- internal/models/User.go
+++ internal/models/User.go
@@ -11,6 +11,9 @@ type User struct {
 	Email     string `gorm:"uniqueIndex;not null"`
 	Password  string `gorm:"not null"`
 	Role      string `gorm:"default:user"`
+	StorageLimit int  `gorm:"default:100"`
+	Plan	string  `gorm:"default:free"`
+	UsedStorage int 
 	CreatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
