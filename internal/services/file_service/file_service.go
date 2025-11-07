37ef99045b4070210003d487062357a5e0635915
Dev-Tams <tammyalemu@gmail.com>
2025-11-07 22:15:54 +0100

    feat add service plan, refactor folder

---
+++ internal/services/file_service/file_service.go
@@ -0,0 +1,80 @@
+package file_service
+
+import (
+	"github.com/dev-tams/file-upload/internal/models"
+	"github.com/dev-tams/file-upload/internal/repositories"
+	"github.com/dev-tams/file-upload/internal/utils"
+
+	"gorm.io/gorm"
+)
+
+type Service struct {
+	repo *repositories.DbRepository
+}
+
+func NewService(repo *repositories.DbRepository) *Service {
+	return &Service{repo: repo}
+}
+
+
+
+func (f *Service) GetAllFiles(id, userID string) ([]utils.FileResponseDTO, error) {
+	files, err := f.repo.GetAllFiles(userID)
+	if err != nil {
+		return nil, err
+	}
+	dto := utils.FromFileModels(files)
+	return dto, nil
+
+}
+
+
+
+func (f *Service) GetFile(id, userID string) (*utils.FileResponseDTO, error) {
+	file, err := f.repo.GetById(id, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := utils.FindFilePath(file); err != nil {
+		return nil, err
+	}
+
+	dto := utils.FromFileModel(*file)
+	return &dto, nil
+}
+
+
+
+
+func (f *Service) DeleteFile(id, userID string) error {
+	
+	return f.repo.WithUserLock(userID, func(tx *gorm.DB, user *models.User) error {
+		var file models.File
+		if err := tx.Where("id = ? AND user_id = ?", id, userID).First(&file).Error; err != nil {
+			return err
+		}
+
+
+		if err := tx.Delete(&file).Error; err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
+
+
+func (f *Service) DownloadFile(id, userID string) (*models.File, error) {
+	file, err := f.repo.GetById(id, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := utils.FindFilePath(file); err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
---
+++ internal/services/file_service/file_service_upload.go
@@ -0,0 +1,81 @@
+package file_service
+
+import (
+	"mime/multipart"
+	"path/filepath"
+	"time"
+
+	"github.com/dev-tams/file-upload/internal/models"
+	"github.com/dev-tams/file-upload/internal/utils"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+func (f *Service) UploadFiles(userID string, files []*multipart.FileHeader) ([]utils.FileResponseDTO, error) {
+	var uploadedFiles []utils.FileResponseDTO
+
+	err := f.repo.WithUserLock(userID, func(tx *gorm.DB, user *models.User) error {
+		//get user storage
+		currentUsage, err := f.repo.GetUserStorageUsage(userID)
+		if err != nil {
+			return nil
+		}
+		//calc storage of upload
+		var totalNewSize int64
+		for _, file := range files {
+			totalNewSize += file.Size
+		}
+
+		//check storage + current file upload
+		if err := utils.EnsureWithinQuota(userID, currentUsage, totalNewSize); err != nil {
+			return err
+		}
+
+		//range files
+		for _, file := range files {
+
+			//validate file by ext and max limit upload
+			if err := utils.ValidateFile(file); err != nil {
+				return err
+			}
+
+			//set file stored name
+			id := uuid.New().String()
+			storedName := id + filepath.Ext(file.Filename)
+
+			//save file in folder
+			savedPath, err := utils.SaveUploadedFile(file, storedName)
+			if err != nil {
+				return err
+			}
+
+			//set file details by model
+			fileModel := models.File{
+				ID:           id,
+				StoredName:   storedName,
+				OriginalName: file.Filename,
+				DisplayName:  file.Filename,
+				UploadedAt:   time.Now(),
+				Size:         file.Size,
+				Path:         savedPath,
+				UserID:       userID,
+			}
+
+			//create file in db
+			if err := f.repo.Create(&fileModel); err != nil {
+				return err
+			}
+
+			//format file response
+			dto := utils.FromFileModel(fileModel)
+			uploadedFiles = append(uploadedFiles, dto)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return uploadedFiles, nil
+}
