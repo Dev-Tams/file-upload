04bd0cb90833ff0e6612032436cf9f20547e210d
Dev-Tams <tammyalemu@gmail.com>
2025-11-14 22:32:03 +0100

    refactor: update file upload service to use storage provider for saving files

--- internal/services/file_service/file_service.go
+++ internal/services/file_service/file_service.go
@@ -3,20 +3,20 @@ package file_service
 import (
 	"github.com/dev-tams/file-upload/internal/models"
 	"github.com/dev-tams/file-upload/internal/repositories"
+	"github.com/dev-tams/file-upload/internal/storage"
 	"github.com/dev-tams/file-upload/internal/utils"
 
 	"gorm.io/gorm"
 )
 
 type Service struct {
-	repo *repositories.DbRepository
+	repo    *repositories.DbRepository
+	storage storage.StorageProvider
 }
 
-func NewService(repo *repositories.DbRepository) *Service {
-	return &Service{repo: repo}
+func NewService(repo *repositories.DbRepository, storage storage.StorageProvider) *Service {
+	return &Service{repo: repo, storage: storage}
 }
-
-
 
 func (f *Service) GetAllFiles(id, userID string) ([]utils.FileResponseDTO, error) {
 	files, err := f.repo.GetAllFiles(userID)
@@ -27,8 +27,6 @@ func (f *Service) GetAllFiles(id, userID string) ([]utils.FileResponseDTO, error
 	return dto, nil
 
 }
-
-
 
 func (f *Service) GetFile(id, userID string) (*utils.FileResponseDTO, error) {
 	file, err := f.repo.GetById(id, userID)
@@ -44,17 +42,13 @@ func (f *Service) GetFile(id, userID string) (*utils.FileResponseDTO, error) {
 	return &dto, nil
 }
 
-
-
-
 func (f *Service) DeleteFile(id, userID string) error {
-	
+
 	return f.repo.WithUserLock(userID, func(tx *gorm.DB, user *models.User) error {
 		var file models.File
 		if err := tx.Where("id = ? AND user_id = ?", id, userID).First(&file).Error; err != nil {
 			return err
 		}
-
 
 		if err := tx.Delete(&file).Error; err != nil {
 			return err
@@ -63,8 +57,6 @@ func (f *Service) DeleteFile(id, userID string) error {
 		return nil
 	})
 }
-
-
 
 func (f *Service) DownloadFile(id, userID string) (*models.File, error) {
 	file, err := f.repo.GetById(id, userID)
